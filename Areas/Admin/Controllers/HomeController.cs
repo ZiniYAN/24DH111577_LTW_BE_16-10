047233ddfe500a8891bda18efe853be29a781412
TQuy <bttquy@gmail.com>
2025-10-19 11:57:55 +0700

    Add project files.

---
+++ Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _24DH111577_LTW_BE_16_10.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_24DH111577_LTW_BE_16_10.MvcApplication" Language="C#" %>
